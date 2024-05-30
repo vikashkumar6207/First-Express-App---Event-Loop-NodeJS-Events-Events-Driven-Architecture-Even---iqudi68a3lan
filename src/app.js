e3aba90e525e25fcc8ab8932e05b9d5c2f59ee99
vikashkumar6207 <vktd828403@gmail.com>
2024-05-30 12:32:13 +0530

    don

--- src/app.js
+++ src/app.js
@@ -2,13 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   // TODO: Implement handling of the root endpoint '/'
+  
+  res.send("Hello World!");
+  
   // Replace this comment with your code to send "Hello World!"
+
+
 });
 
-app.get('', (req, res) => {
+app.get('/about', (req, res) => {
   // TODO: Implement handling of the '/about' endpoint
+  
+  res.send("Welcome to about page");
+  
   // Replace this comment with your code to send "Welcome to about page"
 });
 
